fe6f779f127e7cc529f36ac0b82fdcbfc547ea76
LeanaRain <137782467+LeanaRain@users.noreply.github.com>
2023-11-13 18:47:38 +0500

    feat: add code

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -142,8 +142,8 @@ function getLastDigit(value) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
@@ -159,8 +159,9 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  // корень квадратный из суммы квадратов чисел
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -180,8 +181,9 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const powed = 10 ** pow;
+  return Math.round(num / powed) * powed;
 }
 
 /**
