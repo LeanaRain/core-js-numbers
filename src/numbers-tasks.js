39b683d34bfeb62165286cd34b655adbc04ecf65
LeanaRain <137782467+LeanaRain@users.noreply.github.com>
2023-11-13 21:43:37 +0500

    feat: add code

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -203,8 +203,17 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+/* простое число делится без остатка только на 1 и на само себя */
+function isPrime(n) {
+  if (n <= 1) {
+    return false; // 1 и числа меньше 1 не простые
+  }
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
